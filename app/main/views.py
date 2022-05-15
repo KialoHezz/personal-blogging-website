857907ebe55c6b357742c0cacced55b7b136c797
hezron-ngoma-hezzykialo <hezzykialo.official.ke@gmail.com>
2022-05-15 20:43:27 +0300

    style the navbar

--- app/main/views.py
+++ app/main/views.py
@@ -3,9 +3,13 @@ from flask import render_template
 from . import main
 
 
+
 @main.route('/')
 def index():
     
     title = 'Personal Blog Website'
 
+    
+
     return render_template('index.html', title=title)
+

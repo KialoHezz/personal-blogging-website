3b6d59c5a4965966bf621af8c8231108b9430ee4
hezron-ngoma-hezzykialo <hezzykialo.official.ke@gmail.com>
2022-05-15 23:51:30 +0300

    add flask-script shell and connect to database (persblog)

--- manage.py
+++ manage.py
@@ -1,6 +1,19 @@
-from app import create_app
+from app import create_app,db
+from app.models import User
+from flask_script import Manager
 
+# create instance
 app = create_app()
+
+# create flask-script shell
+manager = Manager(app)
+manager.add_command('db')
+
+
+@manager.shell
+def make_shell():
+    return dict(app=app, db=db, User=User)
+
 
 if __name__ == '__main__':
     app.run()

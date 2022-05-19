c571911217ccd0db67f6dd868d056714b221c58a
hezron-ngoma-hezzykialo <hezzykialo.official.ke@gmail.com>
2022-05-19 11:28:47 +0300

    init deploy

--- config.py
+++ config.py
@@ -12,7 +12,10 @@ class DevConfig(Config):
     DEBUG = True
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    
 
 config_options = {
     'development': DevConfig,
--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from flask_script import Manager,Server
 from  flask_migrate import Migrate, MigrateCommand
 
 # create instance
-app = create_app('development')
+app = create_app('production')
 
 # create flask-script shell
 manager = Manager(app)
